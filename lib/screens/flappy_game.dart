8454a6ebf979fbdd68af078125fb915095e19ef3
Oleg Efimov <efimov.oleg.87@gmail.com>
2021-08-07 01:44:42 +0300

    continuous background, animation, gravity

--- lib/screens/flappy_game.dart
+++ lib/screens/flappy_game.dart
@@ -1,60 +1,63 @@
 import 'dart:async';
-import 'dart:ui' as UI;
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
-import 'package:flutter/material.dart';
 
 class FlappyGame extends BaseGame with TapDetector {
-  late Size screenSize;
-  late List<UI.Image> imagesList;
+  late Vector2 screenSize;
   final bg = SpriteComponent();
+  final bg2 = SpriteComponent();
   final bird = SpriteComponent();
+  double velocityY = 0.5;
 
 
 
   Future<void> onLoad() async {
     bg
       ..sprite = await loadSprite('bg.png')
-        ..size  = size
+        ..size  = screenSize
         ..position = Vector2(0.0, 0.0);
     add(bg);
 
+    bg2
+      ..sprite = await loadSprite('bg.png')
+        ..size  = screenSize
+        ..position = Vector2(screenSize.x, 0.0);
+    add(bg2);
+
     bird
       ..sprite = await loadSprite('bird.png')
-      ..size  = Vector2(40.0, 40.0)
-      ..position = Vector2(50.0, 50.0)
+      ..size  = Vector2(20.0, 20.0)
+      ..position = Vector2(150.0, screenSize.y / 2)
       ..anchor = Anchor.center;
     add(bird);
   }
 
   @override
   void onResize(Vector2 size) {
-    screenSize = size.toSize();
+    print(size);
+    screenSize = size;
     super.onResize(size);
   }
 
-  @override
-  void onTap() {
-    print('tapped');
-  }
 
-  // @override
-  // void render(Canvas canvas) async {
-  //   // Rect testRect = Rect.fromLTWH(10, 20, 300, 400);
-  //   // Paint bgPaint = Paint();
-  //   // bgPaint.color = Colors.lightBlue;
-  //   // canvas.drawRect(testRect, bgPaint);
-  //   UI.Image bgImage = await loadUiImage('assets/images/bg.png');
-  //   // var bgImage = await Sprite.load('assets/images/bg.png');
-  //   Paint bgImagePaint = Paint();
-  //   canvas.drawImage(bgImage, Offset(0.0, 0.0), bgImagePaint);
-  //   super.render(canvas);
-  // }
+  @override
+  void onTapDown(TapDownInfo info) {
+    velocityY -= 3.0;
+  }
 
   @override
   void update(double t) {
     super.update(t);
+    bg.position.x -= 1;
+    bg2.position.x -= 1;
+    if(bg.position.x <= -screenSize.x) {
+      bg.position.x = 0.0;
+      bg2.position.x = screenSize.x;
+    }
+
+    bird.position.y += velocityY;
+    velocityY += 0.1;
   }
 }
